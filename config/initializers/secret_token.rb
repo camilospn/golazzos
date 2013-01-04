7aa70e8ffc606df974c5647036bf38b77aecf3b5
Andres Yepes <afy1987@hotmail.com>
2013-01-03 19:51:24 -0500

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gollazos::Application.config.secret_token = '13b4055b461eaf2982ee940f80b238f1c15efa79d41bb770e5a5c8f3728ccef5544435b1da6c62b1485f2312bda1dcd2a7c60384a6a30265f1a674fad25a0be3'
