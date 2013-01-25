1f1577135a441a435c06ec4ae0b034b807539a81
Andres Yepes <afy1987@hotmail.com>
2013-01-25 17:11:55 -0500

    gem pg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to apuesta_path
+    redirect_to index_apuesta_path
   end
 
   def destroy
