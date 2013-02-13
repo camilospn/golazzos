c486568ad0f0e12b12b6cd2a8495e987dfac2229
Andres Yepes <afy1987@hotmail.com>
2013-02-12 20:15:42 -0500

    BUG EMAIL

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ def self.from_omniauth(auth)
     user.provider = auth.provider
     user.uid = auth.uid
     user.name = auth.info.name
+    #CHAMBONADAAAA!!!!
+    user.email = auth.uid
     user.oauth_token = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
     user.save!    
