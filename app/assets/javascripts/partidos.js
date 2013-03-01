159d80b3781585e146a2890e9d21c894843d7023
Andres Yepes <afy1987@hotmail.com>
2013-02-28 21:38:46 -0500

    AJAX spinner bug removing it

--- app/assets/javascripts/partidos.js
+++ app/assets/javascripts/partidos.js
@@ -1,18 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-
-$(function(){
-
-// hide it first
-$("#spinner").hide();
-
-// when an ajax request starts, show spinner
-$.ajaxStart(function(){
-    $("#spinner").show();
-});
-
-// when an ajax request complets, hide spinner    
-$.ajaxStop(function(){
-    $("#spinner").hide();
-});
