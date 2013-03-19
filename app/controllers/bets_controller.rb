6aa97201ab617ad74da488d6850de7fc845030c4
psagiles <psagiles@ubuntu.(none)>
2013-03-19 07:40:02 -0500

    Correcciones viralidad pag canvas

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -8,14 +8,14 @@ class BetsController < ApplicationController
 			@user.descontar_pezzos(params[:bet][:monto].to_i)
 			@user.aumentar_apuestas
 			@user.consignar_pezzos(5000)
-			flash[:notice] = "Apuesta creada correctamente. Recibes 5000 Pezzos por tu actividad."
+			flash[:notice] = "Juego creada correctamente. Recibes 5000 Pezzos por tu actividad."
 			if params[:bet][:posteo_fb]
-				mensaje= "Acabo de apostar por el marcador "+ params[:bet][:golesLocal]+" - "+params[:bet][:golesVisitante]+" en el partido "+@partido.local+" vs. "+@partido.visitante+". \nJuega con Golazzos y gana fabulosos premios cada mes."
+				mensaje= "Acabo de jugar por el marcador "+@partido.local+" : "+ params[:bet][:golesLocal]+" - "+@partido.visitante+" : "+params[:bet][:golesVisitante]+"\nJuega con Golazzos y gana fabulosos premios cada mes."
 				@user.facebook.put_object("me", "feed", :message => mensaje, :picture => 'https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-snc7/578495_319362508165730_718918976_n.jpg', :link => 'http://www.golazzos.com/', :name => 'Golazzos', :description => 'Golazzos la nueva Plataforma de apuestas sociales en Futbol.')
-  			flash[:notice] = "Gracias por postear en tu muro. Tu apuesta fue creada correctamente. Recibes 5000 Pezzos por tu actividad."
+  			flash[:notice] = "Gracias por postear en tu muro. Tu Juego fue creada correctamente. Recibes 5000 Pezzos por tu actividad."
 			end
 		else
-			flash[:notice] = "No tienes suficientes Pezzos para realizar la apuesta"
+			flash[:notice] = "No tienes suficientes Pezzos para realizar tu juego"
 		end
 		redirect_to partido_path(@partido)
 
