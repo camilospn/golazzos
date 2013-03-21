1cb0d8fab596da079ae24d29ebab4aa4fda3d95b
Andres Yepes <afy1987@hotmail.com>
2013-03-21 09:56:35 -0500

    pagination design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @users= User.paginate(page: params[:page], per_page: 15).order("pezzos DESC")
+    @page = (params[:page] - 1) * 15 
 
 
   end
