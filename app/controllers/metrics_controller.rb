009b82320d7e9784b1055891bf4d2682d02e5e64
Andrés yepes <yepes07@gmail.com>
2013-05-20 09:56:57 -0500

    metrica de apostaria

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -5,7 +5,7 @@ class MetricsController < ApplicationController
     @usuarios_que_apostarian = Profile.where(:apostaria => true).count
     @usuarios_que_apostarian_total = Profile.where('apostaria  IS NOT NULL').count
 
-    @usuarios_que_apostarian_porcentaje = @usuarios_que_apostarian / @usuarios_que_apostarian_total
+    @usuarios_que_apostarian_porcentaje = @usuarios_que_apostarian.to_f / @usuarios_que_apostarian_total.to_f
   end
   
   def emails
@@ -17,7 +17,7 @@ class MetricsController < ApplicationController
       @registro = Metrics.new()
       
       @registro.total_usuarios=User.count
-	  @registro.total_apuestas=Bet.all.count
+	    @registro.total_apuestas=Bet.all.count
 	  @registro.apuestas_usuario=User.average(:bets_number)
 	  @registro.total_referidos=User.where('invitation_id  IS NOT NULL').count
 	  @registro.referidos_usuario=User.average(:referidos)
