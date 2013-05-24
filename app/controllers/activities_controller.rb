296b0559e72a7fc82fd7e8edc23af3d4832c7ea4
Andrés yepes <yepes07@gmail.com>
2013-05-24 11:54:35 -0500

    news feed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@ class ActivitiesController < ApplicationController
   def index
   	following_ids = current_user.following_ids
   	following_ids.push(1)
-  	@activities =PublicActivity::Activity.order("created_at desc").where(owner_id: following_ids, owner_type: "User")
+  	@activities =PublicActivity::Activity.order("created_at desc")
+  	#@activities =PublicActivity::Activity.order("created_at desc").where(owner_id: following_ids, owner_type: "User")
   end
 end
