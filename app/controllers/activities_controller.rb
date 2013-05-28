f4d8770946f77e4320947fb3126f43dde9aeeb6e
Andrés yepes <yepes07@gmail.com>
2013-05-28 15:29:50 -0500

    activity 20%

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,5 +4,9 @@ class ActivitiesController < ApplicationController
   	following_ids.push(1)
   	@activities =PublicActivity::Activity.order("created_at desc")
   	#@activities =PublicActivity::Activity.order("created_at desc").where(owner_id: following_ids, owner_type: "User")
+
+  	@following= current_user.following
+  	@followers= current_user.followers
+  	@ranking_followers=current_user.following.sort_by { |e| -e[:pezzos]}
   end
 end
